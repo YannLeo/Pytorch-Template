e4257ce0261fd6a5fc7ea879dc339297ae704ce5
YannLeo <1070032777@qq.com>
2022-01-13 01:34:59 +0800

    修改时间字符串表示

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def make_dir(info, config):
     if not os.path.exists(save_dir / info['name']):
         os.mkdir(save_dir / info['name'])
     time_list = list(time.localtime(time.time()))
-    time_string = str(time_list[0]) + '-' + str(time_list[1]) + '-' + str(time_list[2]) + '-' + str(time_list[3]) + '-' + str(time_list[4]) + '-' + str(time_list[5])
+    time_string = '-'.join(map(lambda x: str(x).zfill(2), time_list[:6]))
     if not os.path.exists(save_dir / info['name'] / time_string):
         os.mkdir(save_dir / info['name'] / time_string)
         os.mkdir(save_dir / info['name'] / time_string / 'log')
@@ -43,7 +43,6 @@ def main(args):
     os.environ['CUDA_VISIBLE_DEVICES'] = info['device']
     tr = getattr(trainer, info['trainer'])(info, resume, path)
     tr.train()
-    # train('/home/yl/CodeAndData/data/stft_data_2021_12_3_20_17_224_224.h5')
 
 
 if __name__ == '__main__':
